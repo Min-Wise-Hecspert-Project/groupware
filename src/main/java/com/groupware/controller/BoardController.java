152a20b3eaadc8e9a1d815b030b835ab393d189d
pvpvpvpvp <77872841+pvpvpvpvp@users.noreply.github.com>
2022-05-22 21:37:49 +0900

    ㅁㄴㅇ

--- src/main/java/com/groupware/controller/BoardController.java
+++ src/main/java/com/groupware/controller/BoardController.java
@@ -1,7 +1,5 @@
 package com.groupware.controller;
 
-import com.groupware.service.BoardService;
-import com.groupware.vo.BoardVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.groupware.service.BoardService;
+import com.groupware.vo.BoardVO;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -36,7 +37,7 @@ public class BoardController {
 		public String register(BoardVO board, RedirectAttributes rttr) {
 			log.info("register: " + board);
 			service.register(board);
-			rttr.addFlashAttribute("result", board.getBno());
+			rttr.addFlashAttribute("result", board.getBoardIdx());
 			
 			return "redirect:/board/list";
 		}
@@ -51,7 +52,7 @@ public class BoardController {
 		public String modify(BoardVO board, RedirectAttributes rttr) {
 			log.info("modify: " + board);
 			if(service.modify(board)) {
-				rttr.addFlashAttribute("result", board.getBno());
+				rttr.addFlashAttribute("result", board.getBoardIdx());
 			}			
 			
 			return "redirect:/board/list";
