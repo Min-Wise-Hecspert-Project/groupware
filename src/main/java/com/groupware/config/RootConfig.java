415cb8cfe799d63881a302ed28d18af1d7ff5d0d
jmshim <jmshim95@gmail.com>
2022-05-22 11:50:50 +0900

    집에 이클립스 설치했는데 설정정보 뭔가 꼬일것 같음

--- src/main/java/com/groupware/config/RootConfig.java
+++ src/main/java/com/groupware/config/RootConfig.java
@@ -21,13 +21,13 @@ import com.zaxxer.hikari.HikariDataSource;
 @MapperScan(basePackages = {"com.groupware.mapper"})
 public class RootConfig {
 	
-	@Bean // ¸Þ¼ÒµåÀÇ ½ÇÇà °á°ú·Î ¹ÝÈ¯µÇ´Â °´Ã¼´Â ½ºÇÁ¸µ °´Ã¼·Î µî·Ï
+	@Bean // ï¿½Þ¼Òµï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½È¯ï¿½Ç´ï¿½ ï¿½ï¿½Ã¼ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½Ã¼ï¿½ï¿½ ï¿½ï¿½ï¿½
 	public DataSource dataSource() {
 		HikariConfig hikariConfig = new HikariConfig();
 		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
-		hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:XE");
-		hikariConfig.setUsername("scott");
-		hikariConfig.setPassword("tiger"); 
+		hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@3.37.246.102:1521:XE");
+		hikariConfig.setUsername("hecto");
+		hikariConfig.setPassword("hecto"); 
 		
 		return new HikariDataSource(hikariConfig);
 	}
@@ -36,7 +36,7 @@ public class RootConfig {
 	public SqlSessionFactory sqlSessionFactory() throws Exception{
 		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
 		sqlSessionFactory.setDataSource(dataSource());
-//		typeAlias ¼³Á¤ÇÏ±â
+//		typeAlias ï¿½ï¿½ï¿½ï¿½ï¿½Ï±ï¿½
 //		sqlSessionFactory.setTypeAliasesPackage("com.groupware.vo");
 		return (SqlSessionFactory)sqlSessionFactory.getObject();
 	}
@@ -47,7 +47,7 @@ public class RootConfig {
 		resolver.setMaxUploadSize(1024*1024*10);
 		resolver.setMaxUploadSizePerFile(1024*1024*10);
 		resolver.setMaxInMemorySize(1024*1024);
-		//µð½ºÅ©¿¡ ÀúÀåÇÏÁö ¾Ê°í ¸Þ¸ð¸®¿¡ À¯ÁöÇÏµµ·Ï Çã¿ëÇÏ´Â ¹ÙÀÌÆ® ´ÜÀ§ÀÇ ÃÖ´ë ¿ë·®À» ¼³Á¤
+		//ï¿½ï¿½Å©ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ê°ï¿½ ï¿½Þ¸ð¸®¿ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Ïµï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½Ï´ï¿½ ï¿½ï¿½ï¿½ï¿½Æ® ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ö´ï¿½ ï¿½ë·®ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
 		resolver.setUploadTempDir(new FileSystemResource("e:/upload"));
 		resolver.setDefaultEncoding("utf-8");
 		
