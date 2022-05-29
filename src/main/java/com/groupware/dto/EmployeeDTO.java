6a9e33cc2e1b6f6ffd2baa6e756313fb905271ab
jmshim95 <jmshim95@gmail.com>
2022-05-29 17:39:27 +0900

    dto 마저 수정 필요함

--- src/main/java/com/groupware/dto/EmployeeDTO.java
+++ src/main/java/com/groupware/dto/EmployeeDTO.java
@@ -17,63 +17,29 @@ import lombok.ToString;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@RequiredArgsConstructor
 @Alias("EmployeeDTO")
 public class EmployeeDTO {
-	@NonNull
 	private Long employeeIdx;
-	
-	@NonNull
 	private Long companyIdx;
-	
-	@NonNull
 	private Long teamIdx;
-	
-	@NonNull
 	private Long dutyIdx;
-	
-	@NonNull
 	private Long positionIdx;
-	
-	@NonNull
 	private Long authorityIdx;
-	
-	@NonNull
 	private Long departmentIdx;
-	
-	@NonNull
 	private String name;
-	
 	private String enName;
-	
-	@NonNull
 	private String profileImage;
-	
-	@NonNull
 	private String gender;
-	
 	private String birthday;
-	
-	@NonNull
 	private String phoneNumber;
-	
-	@NonNull	
 	private String email;
-	
 	private String address;
-	
 	private String salary;
-	
 	private String annualLeave;
-	
 	private String state;
-	
 	private String hireDate;
-	
 	private String resignationDate;
-	
 	private String insDate;
-	
-	private String updDate;	
+	private String updDate;
 
 }
